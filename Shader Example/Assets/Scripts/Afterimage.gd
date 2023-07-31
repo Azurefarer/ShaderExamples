170f4b564eb3136d65698b61f527bc53bc90abb4
mtchwin <mitchellswinn@gmail.com>
2023-07-30 23:31:20 -0700

    afterimage for sprites

--- Shader Example/Assets/Scripts/Afterimage.gd
+++ Shader Example/Assets/Scripts/Afterimage.gd
@@ -2,7 +2,7 @@ extends GPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	texture = get_parent().texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
