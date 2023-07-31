b3d6a67cdaa5293e9712529aea6d66a9f117c7f7
mtchwin <mitchellswinn@gmail.com>
2023-07-30 22:21:58 -0700

    yes

--- Shader Example/Assets/Scripts/Afterimage_Test_1.gd
+++ Shader Example/Assets/Scripts/Afterimage_Test_1.gd
@@ -1,8 +1,13 @@
-extends Node2D
+extends Control
 
+var startPos = Vector2(0,0)
+var camStartPos = Vector2(0,0)
+var superOngoing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	startPos = $Ken.position
+	camStartPos = $Camera2D.position
 	$ElenaStage/AnimationPlayer.play("BG")
 	$Ken/AnimationPlayer.play("Stance")
 
@@ -10,3 +15,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_reset_pressed():
+	if superOngoing:
+		return
+	$Ken.position = startPos
+	$Camera2D.position = camStartPos
+	$Ken/AnimationPlayer.play("Stance")
+	superOngoing = false
+
+
+func _on_super_1_pressed():
+	if superOngoing:
+		return
+	superOngoing = true
+	$Ken/AnimationPlayer.play("Super1")
+	await get_tree().create_timer(2.2).timeout
+	$Ken/AnimationPlayer.play("Stance")
+	superOngoing = false
