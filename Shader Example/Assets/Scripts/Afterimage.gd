da326850ec66d76d6cca12564f342d8624bab0ee
mtchwin <mitchellswinn@gmail.com>
2023-08-01 02:39:17 -0700

    small changes, added an example default particle shader attempt

--- Shader Example/Assets/Scripts/Afterimage.gd
+++ Shader Example/Assets/Scripts/Afterimage.gd
@@ -7,4 +7,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	process_material.set_shader_parameter("tex_anim_offset",(get_parent().frame+0.0)/get_parent().hframes)
+	doBasicParticleProcessMaterial()
+	process_material.set_shader_parameter("tex_anim_offset",(get_parent().frame+0.0)/(get_parent().hframes-1))
+
+func doBasicParticleProcessMaterial():
+	$GPUParticles2D.process_material.anim_offset_max = (get_parent().frame+0.0)/get_parent().hframes
+	$GPUParticles2D.process_material.anim_offset_min = $GPUParticles2D.process_material.anim_offset_max
